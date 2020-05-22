3e8b37aac37a39008a6a6e5da106f11fc8fe7182
Reika <reikasminecraft@gmail.com>
2020-05-22 11:35:11 -0400

    Update 10 - 0.18

--- prototypes/rubber.lua
+++ prototypes/rubber.lua
@@ -1,56 +1,62 @@
-if not data.raw.item["resin"] or not data.raw.item["rubber"] then
+if data.raw.item["resin"] then
 
-data:extend(
-{
-  {
-    type = "item",
-    name = "resin",
-    icon = "__RubberBelts__/graphics/icons/resin.png",
-	icon_size = 32,
-    flags = {},
-    subgroup = "intermediate-product",
-    order = "f[wood]-f[wood-1-2]",
-    stack_size = 200
-  },
-  {
-    type = "item",
-    name = "rubber",
-    icon = "__RubberBelts__/graphics/icons/rubber.png",
-	icon_size = 32,
-    flags = {},
-    subgroup = "intermediate-product",
-    order = "f[wood]-f[wood-1-2]",
-    stack_size = 200
-  },
-})
+else
+	data:extend(
+	{
+	  {
+		type = "item",
+		name = "resin",
+		icon = "__RubberBelts__/graphics/icons/resin.png",
+		icon_size = 32,
+		flags = {},
+		subgroup = "intermediate-product",
+		order = "f[wood]-f[wood-1-2]",
+		stack_size = 200
+	  },
+	  {
+		type = "recipe",
+		name = "resin",
+		--ingredients = {{"wood", 1}},
+		--result = "resin",
+		--result_count = 3
+		normal = {
+			ingredients = {{"wood", 1}},
+			result = "resin",
+			result_count = 3
+		},
+		expensive = {
+			ingredients = {{"wood", 2}},
+			result = "resin",
+			result_count = 3
+		},
+	  }
+	})
+end
 
-data:extend(
-{
-  {
-    type = "recipe",
-    name = "resin",
-    --ingredients = {{"wood", 1}},
-    --result = "resin",
-    --result_count = 3
-	normal = {
-		ingredients = {{"wood", 1}},
-		result = "resin",
-		result_count = 3
-	},
-	expensive = {
-		ingredients = {{"wood", 2}},
-		result = "resin",
-		result_count = 3
-	},
-  },
-  {
-    type = "recipe",
-    name = "rubber",
-    category = "smelting",
-    energy_required = 2.5,
-    ingredients = {{"resin", 2}},
-    result = "rubber"
-  },
-})
-
+if data.raw.item["rubber"] then
+	data.raw.item.rubber.icon = "__RubberBelts__/graphics/icons/rubber.png"
+	data.raw.item.rubber.icon_size = 32
+	data.raw.item.rubber.icon_mipmaps = 0
+else
+	data:extend(
+	{
+	  {
+		type = "item",
+		name = "rubber",
+		icon = "__RubberBelts__/graphics/icons/rubber.png",
+		icon_size = 32,
+		flags = {},
+		subgroup = "intermediate-product",
+		order = "f[wood]-f[wood-1-2]",
+		stack_size = 200
+	  },
+	  {
+		type = "recipe",
+		name = "rubber",
+		category = "smelting",
+		energy_required = 2.5,
+		ingredients = {{"resin", 2}},
+		result = "rubber"
+	  }
+	})
 end
